3ec482f033b816622581e04f9bfeb2b7a4a81d3a
Brian Harvey <brian.harv3y@gmail.com>
2020-10-02 10:32:55 -0700

    fix typo in 'view' directory to 'views' directory

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
+require('pry')
 require('./lib/definition')
 also_reload('lib/**/*.rb')
 
@@ -21,9 +22,9 @@ end
 
 post('/words') do
   name = params[:word_name]
-  word = Word.new({:name => params[:word_name], :id => nil})
+  word = Word.new({:name => name, :id => nil})
   word.save()
-  words = Word.all
+  @words = Word.all
   erb(:words)
 end
 
